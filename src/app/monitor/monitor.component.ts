2d43b621895de99fd87ab1ef4ecb1c30677738cb
Szegedi Dávid <szegedi.david@innobyte.hu>
2020-02-05 14:10:58 +0100

    Chart coloring by last value

--- src/app/monitor/monitor.component.ts
+++ src/app/monitor/monitor.component.ts
@@ -4,15 +4,30 @@ import { ChartDataSets } from 'chart.js';
 import { Color, Label } from 'ng2-charts';      // canvasjs.min.js is 130 K smaller
 import { interval } from 'rxjs';
 
+class VoltageLevel {
+  constructor(public value: number, public chartColor: string) {
+  }
+}
+
 @Component({
   selector: 'app-monitor',
   templateUrl: './monitor.component.html',
   styleUrls: ['./monitor.component.css']
 })
 export class MonitorComponent implements OnInit, OnDestroy {
+
+  // voltage levels for chart coloring
+  static voltageLevels: VoltageLevel[] = [
+    new VoltageLevel(1, 'rgba(0,0,255,0.28)'),
+    new VoltageLevel(2, 'rgba(0,255,0,0.28)'),
+    new VoltageLevel(3, 'rgba(255,0,0,0.28)')
+  ];
+
   private UPDATE_INTERVAL = 500;
   public maxDataLength = 50;
   private intervalSubscription;
+
+  private prevVoltageLevel: VoltageLevel = MonitorComponent.voltageLevels[1];
 
   lineChartData: ChartDataSets[] = [
     {data: [], label: 'Voltage'}
@@ -27,7 +42,7 @@ export class MonitorComponent implements OnInit, OnDestroy {
   lineChartColors: Color[] = [
     {
       borderColor: 'black',
-      backgroundColor: 'rgba(0,255,0,0.28)'
+      backgroundColor: this.prevVoltageLevel.chartColor
     }
   ];
 
@@ -56,11 +71,31 @@ export class MonitorComponent implements OnInit, OnDestroy {
         this.lineChartLabels.shift();
       }
 
+      this.checkVoltageLevel(data.value);
+
     });   // todo error
+  }
+
+  checkVoltageLevel(lastValue: number) {
+    let newVoltageLevel: VoltageLevel;
+    if (lastValue <= MonitorComponent.voltageLevels[0].value) {
+      newVoltageLevel = MonitorComponent.voltageLevels[0];
+    } else if (lastValue > MonitorComponent.voltageLevels[0].value && lastValue < MonitorComponent.voltageLevels[2].value) {
+      newVoltageLevel = MonitorComponent.voltageLevels[1];
+    } else if (lastValue >= MonitorComponent.voltageLevels[2].value) {
+      newVoltageLevel = MonitorComponent.voltageLevels[2];
+    }
+
+    if (newVoltageLevel !== this.prevVoltageLevel) {
+      this.prevVoltageLevel = newVoltageLevel;
+      this.lineChartColors[0].backgroundColor = newVoltageLevel.chartColor;
+      // todo audio
+    }
   }
 
   ngOnDestroy(): void {
     this.intervalSubscription.unsubscribe();
   }
-
 }
+
+
--- src/app/service/main.service.ts
+++ src/app/service/main.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { ControllerModel, MonitorModel } from '../models/wemos.model';
+import { ControllerModel, MonitorModel } from '../model/wemos.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 
